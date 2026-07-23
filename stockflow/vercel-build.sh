7b8d37145167faf585340f94f0b83d4a8c32a4e1
krushna9506 <krushna9506@gmail.com>
2026-07-23 15:44:44 +0530

    Add Vercel automated Flutter build configuration

---
+++ stockflow/vercel-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "==> Downloading Flutter SDK for Vercel..."
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+echo "==> Fetching Flutter dependencies..."
+flutter pub get
+
+echo "==> Building Flutter Web Release..."
+flutter build web --release
